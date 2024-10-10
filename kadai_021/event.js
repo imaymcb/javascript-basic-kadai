7bbae1bf219adaee02255d66dbbd9af79aa45998
imaymcb <imaymcb@gmail.com>
2024-10-10 17:11:38 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('btn');
-
-button.addEventListener('click', () => {
-  console.log(document.text)
-  setTimeout(() => {
-    text.textContent = 'ボタンをクリックしました';
-  }, 2000);
-});
